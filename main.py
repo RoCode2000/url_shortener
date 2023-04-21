26bf4c054f4ca528c46f767b9f568b4c5d067e8c
Roger Teong <rogerteong@Rogers-MacBook-Air.local>
2023-04-22 01:35:46 +0800

    Changes to templates and main

--- main.py
+++ main.py
@@ -33,6 +33,7 @@ class UrlMapping(Base):
     id = Column(Integer, primary_key=True, index=True)
     original_url = Column(String, index=True)
     short_url_key = Column(String, index=True, unique=True)
+    short_url = Column(String, index=True, unique=True)
     clicks = Column(Integer, default=0)
     is_active = Column(Boolean, default=True)
 
@@ -73,39 +74,42 @@ app.add_middleware(
 
 @app.get("/")
 async def root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request, "button_text": "URL-Shortener"})
+    return templates.TemplateResponse("index.html", {"request": request, "url_shortener": "URL-Shortener", "reverse_shortener": "Reverse-URL-Shortener"})
 
 
 @app.get("/url/")
-async def read_root(request: Request):
-    return templates.TemplateResponse("urlshort.html", {"request": request})
+async def read_root(request: Request, db: Session = Depends(get_db)):
+    all_short_url = db.query(UrlMapping).all()
+    for items in all_short_url:
+        print(items)
+    return templates.TemplateResponse("urlshort.html", {"request": request, "all_short_url": all_short_url})
 
 
 @app.post("/url/submit")
 async def get_url(og_url: Item, db: Session = Depends(get_db)):
-    print(og_url)
     original_url = og_url.url
 
-    if validators.url(original_url):
-        hash_object = hashlib.md5(original_url.encode())
-        hash_str = hash_object.hexdigest()
-        short_url_key = create_random_key(6) + "_" + hash_str[:6]
-
-        url_mapping = UrlMapping(
-            original_url=original_url, short_url_key=short_url_key)
-        db.add(url_mapping)
-        db.commit()
-        db.refresh(url_mapping)
-        print(url_mapping)
-        return url_mapping
-    else:
+    if not validators.url(original_url):
         return HTTPException(status_code=400, detail="Your provided URL is not valid")
 
+    hash_object = hashlib.md5(original_url.encode())
+    hash_str = hash_object.hexdigest()
+    short_url_key = create_random_key(6) + "_" + hash_str[:6]
+    short_url = hashlib.md5(short_url_key.encode()).hexdigest()[:6]
 
-@app.get("/url/{short_url_key}")
-async def forward_to_target_url(short_url_key: str, db: Session = Depends(get_db)):
+    url_mapping = UrlMapping(
+        original_url=original_url, short_url_key=short_url_key, short_url=short_url)
+    db.add(url_mapping)
+    db.commit()
+    db.refresh(url_mapping)
+
+    return url_mapping
+
+
+@app.get("/url/{short_url}")
+async def forward_to_target_url(short_url: str, db: Session = Depends(get_db)):
     url_mapping = db.query(UrlMapping).filter(
-        UrlMapping.short_url_key == short_url_key).filter(UrlMapping.is_active == True).first()
+        UrlMapping.short_url == short_url).filter(UrlMapping.is_active == True).first()
     if url_mapping:
         url_mapping.clicks += 1
         db.commit()
@@ -114,14 +118,14 @@ async def forward_to_target_url(short_url_key: str, db: Session = Depends(get_db
         return HTTPException(status_code=404, detail="URL does not exist")
 
 
-@app.delete("/url/{short_url_key}")
-async def disable_target_url(short_url_key: str, db: Session = Depends(get_db)):
+@app.delete("/url/{short_url}")
+async def disable_target_url(short_url: str, db: Session = Depends(get_db)):
     url_mapping = db.query(UrlMapping).filter(
-        UrlMapping.short_url_key == short_url_key).filter(UrlMapping.is_active == True).first()
+        UrlMapping.short_url == short_url).filter(UrlMapping.is_active == True).first()
     if url_mapping:
         url_mapping.is_active = False
         db.commit()
-        message = f"Successfully deleted shortened URL for key: {short_url_key}"
+        message = f"Successfully deleted shortened URL for key: {short_url}"
         return {"detail": message}
     else:
         return HTTPException(status_code=404, detail="URL does not exist")
